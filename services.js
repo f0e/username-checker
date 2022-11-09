b62d1637955be10f8e5b70740adca39122a55b8a
f0e <7321764+f0e@users.noreply.github.com>
2022-11-10 00:53:02 +1000

    update services.js

--- services.js
+++ services.js
@@ -1,6 +1,6 @@
 /*
-	template:
-	{
+  template:
+  {
     "name": string, website display name
     "url": query url, can be a function that takes in the current word
     "is_available": function to validate that a username is available
@@ -10,7 +10,7 @@
 
     "request_method": defaults to get
     "request_data": can contain functions that take in the current word
-	}
+  }
 */
 
 export default [
